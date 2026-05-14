692086280baa0d3ef0eddaccdbe7842f17c87e16
Varaprasad Bandi <varaprasadbandi777@gmail.com>
2026-05-14 14:38:07 +0530

    Final Industry Refinements: Env Security, Logging, and AuthContext

--- src/main.jsx
+++ src/main.jsx
@@ -5,8 +5,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import './index.css';
 
+import { AuthProvider } from './context/AuthContext.jsx';
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>,
 );
